3f15d344fa1e9255d8ca562a649ca3566e5d66eb
Harry Phillips <harry@tux.com.au>
2026-06-27 12:15:51 +1000

    Postgres has setting table

--- data_sync.py
+++ data_sync.py
@@ -166,6 +166,15 @@ def sync():
             """, (pg_host_id, ns['last_connectivity_check'], bool(ns['is_connected'])))
             pg_conn.commit()
 
+        # Sync ping hosts to remote DB
+        cursor.execute("SELECT setting, value FROM setting WHERE setting LIKE 'ping_host_%'")
+        for row in cursor.fetchall():
+            pg_cursor.execute(
+                "INSERT INTO setting (setting, value) VALUES (%s, %s) ON CONFLICT (setting) DO UPDATE SET value = EXCLUDED.value",
+                (row['setting'], row['value'])
+            )
+        pg_conn.commit()
+
         sqlite_conn.execute(
             "UPDATE setting SET value = datetime('now') WHERE setting = 'last_db_sync'"
         )
---
+++ initdb/08_setting.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS setting (
+    setting  TEXT PRIMARY KEY,
+    value    TEXT
+);
