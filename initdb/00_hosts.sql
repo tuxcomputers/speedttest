9f165257e4d39db8f6878a97812e651a68e76f14
Harry Phillips <harry@tux.com.au>
2026-06-27 11:03:58 +1000

    Host table

--- initdb/00_hosts.sql
+++ initdb/00_hosts.sql
@@ -1,5 +1,7 @@
 CREATE TABLE IF NOT EXISTS host (
-    host_id  SERIAL PRIMARY KEY,
-    hostname TEXT NOT NULL UNIQUE,
-    timezone TEXT NOT NULL DEFAULT 'UTC'
+    host_id   SERIAL PRIMARY KEY,
+    hostname  TEXT NOT NULL UNIQUE,
+    timezone  TEXT NOT NULL DEFAULT 'UTC',
+    host_hash TEXT,
+    remote_id TEXT
 );
